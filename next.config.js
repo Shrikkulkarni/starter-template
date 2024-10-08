973565ae0849362ab618a8d1819e3922495fcc6f
“SK” <shrikant@chinchilla-ai.com>
2024-10-08 15:49:00 +0530

    fixed next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 const nextConfig = {
   eslint: {
-      ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   // swcMinify: false,
 };
